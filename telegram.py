c106a5b74639260b3a87c4a2335bc1b9b77b5b0b
Marco Accorinti <accosk8@gmail.com>
2015-07-06 12:20:19 +0200

    Rifattorizzato un po' tutto Rimossa ogni reference a urllib in favore di Requests e altra roba

--- telegram.py
+++ telegram.py
@@ -1,40 +1,58 @@
 __author__ = 'Matteo De Marie'
 
-import urllib.request as urlr
+import sys
+import requests
 import json
 import mylib.manage_json as mj
 import wowheadbot as bot
 import time
 
-t = open("key.token", "r")
-token = t.readline()
-last_id = 0
 
-url1 = "https://api.telegram.org/bot" + token + "/getupdates"
-urlok1 = urlr.urlopen(url1)
-line1 = urlok1.read().decode('utf8')
-json_data = json.loads(line1)
+def main():
+    token = extract_token("key.token")
+    get_updates_response = requests.get("https://api.telegram.org/bot" + token + "/getupdates")
 
-while 1:
-    time.sleep(2)
-    url = "https://api.telegram.org/bot" + token + "/getupdates?offset=" + str(last_id)
-    #print (url)
-    urlok = urlr.urlopen(url)
-    line = urlok.read().decode('utf8')
-    json_data = json.loads(line)
+    if get_updates_response.status_code is 200:
+        json_data = json.loads(get_updates_response.text)
+        json.dump(json_data, sys.stdout, sort_keys=True, indent=4, separators=(',', ': '))
+        print(json_data)
+        get_updates_response.close()  # non usare piu' get_updates_response
+        check_for_updates(token)
 
-    if len(json_data['result']) != 1:
-        if mj.message_ok(json_data):
+
+def extract_token(filename):
+    t = open(filename, "r")
+    token = t.readline()
+    return token
+
+
+def check_for_updates(token):
+    with open("last_id", "r") as f:
+        last_id = f.readline()
+
+    while True:
+        time.sleep(2)  # non riempire di richieste il server
+        get_updates_request = "https://api.telegram.org/bot" + token + "/getupdates?offset=" + str(last_id)
+        json_data = json.loads(requests.get(get_updates_request).text)
+
+        if len(json_data['result']) != 1 and mj.message_ok(json_data):
             for i in range(1, len(json_data['result'])):
                 if "/wowhead" in json_data['result'][i]['message']['text']:
                     comando = json_data['result'][i]['message']['text']
-                    ssearch = comando.replace("/wowhead ","")
+                    ssearch = comando.replace("/wowhead ", "")
 
                     chat_id = json_data['result'][i]['message']['chat']['id']
                     message = bot.cerca(ssearch)
-                    url_msg = "https://api.telegram.org/bot" + token + "/sendmessage?chat_id=" + str(chat_id) + "&text=" + message
-                    print (url_msg)
-                    urlr.urlopen(url_msg)
+                    send_message_request = "https://api.telegram.org/bot" + token + "/sendmessage?chat_id=" + str(
+                        chat_id) + "&text=" + message
 
-            last_id = json_data['result'][len(json_data['result'])-1]['update_id']
+                    print (send_message_request)
+                    requests.post(send_message_request)
 
+            last_id = json_data['result'][len(json_data['result']) - 1]['update_id']
+            with open("last_id", "w") as f:
+                f.write(str(last_id))
+
+
+if __name__ == '__main__':
+    main()
--- wowheadbot.py
+++ wowheadbot.py
@@ -1,33 +1,39 @@
-import urllib.request as urlr
+import requests
 import json
 import mylib.manage_json as mj
 import mylib.link_gen as lg
 
 
 def cerca(stringa):
-    stringa_form = stringa.replace(" ","+")
+    print("searching for '%s'..." % stringa)
+    stringa_form = stringa.replace(" ", "+")
 
     #costruisco l'url corretto e richiedo la pagina
     url = "http://www.wowhead.com/search?q=" + stringa_form
-    urlok = urlr.urlopen(url)
-    f = open ("temp.txt", "w")
+    print("search url is '%s'" % url)
+    wowhead_search_result_response = requests.get(url)
     #faccio la scansione della pagina fino a trovare la variabile
     #che contiene il json con i risultati della query
-    line = urlok.readline()
-
+    response_text = wowhead_search_result_response.text
+    search_type = "none"
+    f = open("wowhead_response.html", "w")
+    f.write(response_text.encode('utf-8'))
+    f.close()
+    f = open("wowhead_response.html", "r")
+    line = f.readline()
     while line:
-        line = urlok.readline().decode('utf8')
-        f.write(line)
         if "popTop10" in line:
             search_type = "list"
             break
         elif '<meta property="og&#x3A;url" content="http&#x3A;&#x2F;&#x2F;www.wowhead.com&#x2F;' in line:
             search_type = "direct"
             break
-        else:
-            return "Oggetto non trovato"
+        line = f.readline()
 
-    json_string = mj.cut_json(line,search_type)
+    if search_type is "none":
+        return "not_found"
+
+    json_string = mj.cut_json(line, search_type)
 
     if search_type == "direct":
         return json_string
